4c824ae07f37d5630eb291bf55c33ecaf6576429
Marcin Pisz <marcinpisz@gmail.com>
2016-10-27 23:29:21 -0600

    Finished but not working and not sure why

--- app/src/main/java/com/example/marcin/optionsmenu/MainActivity.java
+++ app/src/main/java/com/example/marcin/optionsmenu/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.options_menu, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
         return true;
     }
 
