f768ed2ae12de65039d83df5376e61f3615c056f
Marcin Pisz <marcinpisz@gmail.com>
2016-10-27 23:20:10 -0600

    Finished but not working

--- app/src/main/java/com/example/marcin/optionsmenu/MainActivity.java
+++ app/src/main/java/com/example/marcin/optionsmenu/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.marcin.optionsmenu;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,23 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+     //   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+     //   fab.setOnClickListener(new View.OnClickListener() {
+    //        @Override
+     //       public void onClick(View view) {
+     //           Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+     //                   .setAction("Action", null).show();
             }
-        });
-    }
+     //   };
+    //}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
     }
 
@@ -43,10 +45,28 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        switch (id) {
+            case R.id.action_profile:
+                showAlertDialog("Profile", "You selected profile");
+                return true;
 
-        return super.onOptionsItemSelected(item);
+            case R.id.action_capture:
+                showAlertDialog("Settings", "You selected settings");
+                return true;
+
+            case R.id.action_about:
+                showAlertDialog("About", "You selected about");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    private void showAlertDialog(String title, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.show();
     }
 }
